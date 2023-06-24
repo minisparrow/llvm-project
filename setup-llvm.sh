135137577943ecfa788556a9edaf43b3206e04fa
minisparrow <melchior_faith@163.com>
2023-06-24 10:04:36 +0800

    build script

--- setup-llvm.sh
+++ setup-llvm.sh
@@ -1,13 +1,16 @@
 mkdir -p ./build && cd ./build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo\
-               -DLLVM_TARGETS_TO_BUILD="RISCV;X86" \
-               -DLLVM_ENABLE_PROJECTS="clang;mlir" \
-               -DLLVM_BUILD_LLVM_DYLIB=ON \
-               -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-               -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-               -DLLVM_USE_LINKER=gold \
-               -DLLVM_BUILD_EXAMPLES=ON \
-               ../llvm
-ninja -j10  
-
-
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+            -DLLVM_TARGETS_TO_BUILD="RISCV;X86;NVPTX" \
+            -DMLIR_ENABLE_CUDA_RUNNER=ON \
+            -DCMAKE_CUDA_COMPILER:PATH=/usr/local/cuda/bin/nvcc \
+            -DLLVM_ENABLE_PROJECTS="clang;mlir" \
+            -DLLVM_INSTALL_UTILS=ON \
+            -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+            -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+            -DLLVM_USE_LINKER=gold \
+            -DLLVM_INCLUDE_EXAMPLES=ON \
+            -DLLVM_BUILD_EXAMPLES=ON \
+            -DLLVM_INSTALL_UTILS=ON \
+            -DMLIR_INCLUDE_INTEGRATION_TESTS=ON \
+            ../llvm
+ninja -j10
